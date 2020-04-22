2ce059a1cd60a60508121210b6fa79fcc6763bb7
Kamil Rzeszutek <48318419+kamiljx@users.noreply.github.com>
2020-04-23 01:12:35 +0200

    v.0002

--- app.js
+++ app.js
@@ -7,18 +7,139 @@ var fieldType;
     fieldType[fieldType["checkBox"] = 5] = "checkBox";
     fieldType[fieldType["select"] = 6] = "select";
 })(fieldType || (fieldType = {}));
-var textInput = /** @class */ (function () {
-    function textInput(name, label, type) {
+//NAME
+var username = /** @class */ (function () {
+    function username(name, label) {
+        this.name = name;
+        this.type = fieldType.textBox;
         this.e = document.createElement('input');
-        this.name = name,
-            this.label = label,
-            this.type = 1;
+        this.e.name = this.name;
+        this.labelHTML = document.createElement('label');
+        this.labelHTML.innerHTML = label;
+        this.labelHTML.htmlFor = name;
+        this.label = label;
     }
-    textInput.prototype.render = function () {
+    username.prototype.render = function () {
         return this.e;
     };
-    textInput.prototype.getValue = function () {
+    username.prototype.getValue = function () {
         return this.e.value;
     };
-    return textInput;
+    return username;
 }());
+//DO YOU PREFER E-LEARNING
+var elearning = /** @class */ (function () {
+    function elearning(name, label) {
+        this.name = name;
+        this.type = fieldType.checkBox;
+        this.e = document.createElement('input');
+        this.e.name = this.name;
+        this.labelHTML = document.createElement('label');
+        this.labelHTML.innerHTML = label;
+        this.labelHTML.htmlFor = name;
+        this.label = label;
+    }
+    elearning.prototype.render = function () {
+        return this.e;
+    };
+    elearning.prototype.getValue = function () {
+        return this.e.value;
+    };
+    return elearning;
+}());
+//Field of study 
+var studyField = /** @class */ (function () {
+    function studyField(name, label) {
+        this.name = name;
+        this.type = fieldType.select;
+        this.e = document.createElement('input');
+        this.e.name = this.name;
+        this.labelHTML = document.createElement('label');
+        this.labelHTML.innerHTML = label;
+        this.labelHTML.htmlFor = name;
+        this.label = label;
+    }
+    studyField.prototype.render = function () {
+        return this.e;
+    };
+    studyField.prototype.getValue = function () {
+        return this.e.value;
+    };
+    return studyField;
+}());
+//COMMENTS
+var comments = /** @class */ (function () {
+    function comments(name, label) {
+        this.element = document.createElement('input');
+        this.name = name;
+        this.label = label;
+        this.type = fieldType.textArea;
+        this.element.name = this.name;
+        this.element.type = 'textArea';
+    }
+    comments.prototype.render = function () {
+        return this.element;
+    };
+    comments.prototype.getValue = function () {
+        return this.element.value;
+    };
+    return comments;
+}());
+//email
+var EmailField = /** @class */ (function () {
+    function EmailField(name, label) {
+        this.name = name;
+        this.type = fieldType.textArea;
+        this.e = document.createElement('input');
+        this.e.name = this.name;
+        this.labelHTML = document.createElement('label');
+        this.labelHTML.innerHTML = label;
+        this.labelHTML.htmlFor = name;
+        this.label = label;
+    }
+    EmailField.prototype.render = function () {
+        return this.e;
+    };
+    EmailField.prototype.getValue = function () {
+        return this.e.value;
+    };
+    return EmailField;
+}());
+var Form = /** @class */ (function () {
+    function Form(id, idValue) {
+        this.fields = new Array();
+        this.formElement = document.getElementById(id);
+    }
+    Form.prototype.render = function () {
+        var _this = this;
+        this.fields.forEach(function (e) {
+            _this.formElement.appendChild(e.labelHTML);
+            _this.formElement.appendChild(e.render());
+        });
+    };
+    Form.prototype.getValue = function () {
+        // TODO: pętla wyświetlająca wartości kolejnych pól
+    };
+    return Form;
+}());
+var pushApp = /** @class */ (function () {
+    function pushApp() {
+        var _a;
+        var e = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            e[_i] = arguments[_i];
+        }
+        this.form = new Form('container', 'answer');
+        (_a = this.form.fields).push.apply(_a, e);
+    }
+    pushApp.prototype.renderApp = function () {
+        this.form.render();
+    };
+    return pushApp;
+}());
+var textBox = new username('username', 'FIRST NAME & SURNAME');
+var email = new EmailField('email', 'email');
+document.getElementById('addForm').addEventListener('click', function () {
+    var renderForm = new pushApp(textBox, email);
+    renderForm.renderApp();
+});
--- app.ts
+++ app.ts
@@ -11,21 +11,28 @@ interface Field {
     name: string;
     label: string;
     type: fieldType;
+    labelHTML: HTMLLabelElement;
     render(): HTMLElement;
     getValue(): any;
 }
 
-class textInput implements Field {
+//NAME
+class username implements Field {
     name: string;
     label: string;
     type: fieldType;
     e: HTMLInputElement;
+    labelHTML: HTMLLabelElement;
 
     constructor(name: string, label: string){
-        this.e = document.createElement('input') as HTMLInputElement
-        this.name = name,
-        this.label = label,
-        this.e.name = this.name
+        this.name = name;
+        this.type = fieldType.textBox;
+        this.e = <HTMLInputElement>document.createElement('input');
+        this.e.name = this.name;
+        this.labelHTML = <HTMLLabelElement>document.createElement('label');
+        this.labelHTML.innerHTML = label;
+        this.labelHTML.htmlFor = name;
+        this.label = label;
     }
 
     render(): HTMLElement{
@@ -36,16 +43,168 @@ class textInput implements Field {
     }
 }
 
+//DO YOU PREFER E-LEARNING
+class elearning implements Field {
+    name: string;
+    label: string;
+    type: fieldType;
+    e: HTMLInputElement;
+    labelHTML: HTMLLabelElement;
+
+
+    constructor(name:string, label:string) {
+        this.name = name;
+        this.type = fieldType.checkBox;
+        this.e = <HTMLInputElement>document.createElement('input');
+        this.e.name = this.name;
+        this.labelHTML = <HTMLLabelElement>document.createElement('label');
+        this.labelHTML.innerHTML = label;
+        this.labelHTML.htmlFor = name;
+        this.label = label;
+
+    }
+
+    render(): HTMLElement {
+        return this.e;
+    }
+    getValue(): any {
+        return this.e.value
+    }
+
+}
+
+//Field of study 
+class studyField implements Field {
+    name: string;
+    label: string;
+    type: fieldType;
+    e: HTMLInputElement;
+    labelHTML: HTMLLabelElement;
+
+
+    constructor(name:string, label:string) {
+        this.name = name;
+        this.type = fieldType.select;
+        this.e = <HTMLInputElement>document.createElement('input');
+        this.e.name = this.name;
+        this.labelHTML = <HTMLLabelElement>document.createElement('label');
+        this.labelHTML.innerHTML = label;
+        this.labelHTML.htmlFor = name;
+        this.label = label;
+
+    }
+
+    render(): HTMLElement {
+        return this.e;
+    }
+    getValue(): any {
+        return this.e.value
+    }
+
+}
+
+//COMMENTS
+class comments implements Field {
+    name: string;
+    label: string;
+    type: fieldType;
+    element: HTMLInputElement;
+    labelHTML: HTMLLabelElement;
+
+
+
+    constructor(name:string, label:string) {
+        this.element = <HTMLInputElement>document.createElement('input');
+        this.name = name;
+        this.label = label;
+        this.type= fieldType.textArea;
+        this.element.name =this.name;
+        this.element.type = 'textArea';
+
+    }
+
+    render(): HTMLElement {
+        return this.element;
+    }
+    getValue(): any {
+        return this.element.value
+    }
+
+}
+
+//email
+class EmailField implements Field {
+    name: string;
+    label: string;
+    type: fieldType;
+    e: HTMLInputElement;
+    labelHTML: HTMLLabelElement;
+
+
+
+    constructor(name:string, label:string) {
+        this.name = name;
+        this.type = fieldType.textArea;
+        this.e = <HTMLInputElement>document.createElement('input');
+        this.e.name = this.name;
+        this.labelHTML = <HTMLLabelElement>document.createElement('label');
+        this.labelHTML.innerHTML = label;
+        this.labelHTML.htmlFor = name;
+        this.label = label;
+
+    }
+
+    render(): HTMLElement {
+        return this.e;
+    }
+    getValue(): any {
+        return this.e.value
+    }
+
+}
+
+
+
+
 class Form {
     fields: Field[];
     formElement: HTMLElement;
-constructor(id: string) {
-this.fields = new Array();
-this.formElement = document.getElementById(id);
-}
-    render(): void {
-     // TODO: pętla wyświetlająca kolejne pola
-}
-getValue(): void {
-// TODO: pętla wyświetlająca wartości kolejnych pól
-} }
+    elementContent: HTMLElement;
+
+    constructor(id: string, idValue: string) {
+        this.fields = new Array();
+        this.formElement = document.getElementById(id);
+    }
+    render(): void {    
+        this.fields.forEach(e => {
+            this.formElement.appendChild(e.labelHTML);
+            this.formElement.appendChild(e.render())
+            
+        });
+    }
+    
+    
+    getValue(): void {
+        // TODO: pętla wyświetlająca wartości kolejnych pól
+    } }
+
+    class pushApp{
+        form: Form;
+        
+        constructor(...e: Field[]) {
+            this.form = new Form ('container', 'answer')
+            this.form.fields.push(...e)
+        }
+        renderApp(){
+            this.form.render();
+        }
+    }
+
+
+    const textBox = new username('username', 'FIRST NAME & SURNAME')
+    const email = new EmailField('email', 'email')
+
+     document.getElementById('addForm').addEventListener('click', function(){
+         const renderForm = new pushApp(textBox, email)
+         renderForm.renderApp()
+     })
